6261520fcf532cf783d94dce384a851551841838
MikeZhang <p51729@yahoo.com.tw>
2019-03-19 13:44:51 +0800

    Initial Commit

---
+++ 多功能APP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  多功能APP
+//
+//  Created by 張仕錦 on 2019/3/19.
+//  Copyright © 2019 AppsMike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
